69a0e34c40314c23780738a4455790e5d8fb3c06
Predrag Vlajkovic drPreAG <predrag.vlajkovic@gmail.com>
2018-12-23 13:56:35 +0100

    Fixed Add view

--- src/components/Add.vue
+++ src/components/Add.vue
@@ -14,14 +14,14 @@
     </div>
     <br>    
     <div>
-      <router-link :to="{ name:'AddX', params: { prop_title:'Непознат број до 5', prop_min_1:3, prop_max_1:5, prop_min_2:1, prop_max_2:4 } }">
-        <button class="btn btn-info">Непознат број до 5</button>
+      <router-link :to="{ name:'AddX', params: { prop_title:'Непознат број до 10', prop_min_1:6, prop_max_1:10, prop_min_2:2, prop_max_2:5 } }">
+        <button class="btn btn-info">Непознат број до 10</button>
       </router-link>    
     </div>
     <br>      
     <div>
-      <router-link :to="{ name:'AddX', params: { prop_title:'Непознат број до 10', prop_min_1:5, prop_max_1:10, prop_min_2:2, prop_max_2:6 } }">
-        <button class="btn btn-info">Непознат број до 10</button>
+      <router-link :to="{ name:'AddX', params: { prop_title:'Непознат број до 20', prop_min_1:11, prop_max_1:20, prop_min_2:2, prop_max_2:10 } }">
+        <button class="btn btn-info">Непознат број до 20</button>
       </router-link>    
     </div>    
 
