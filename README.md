1b033d0346712f1b27c7b2b477a922afc72b5841
Predrag Vlajkovic <32838285+drpreag@users.noreply.github.com>
2022-03-28 01:35:46 +0200

    Prepare for new version (#10)
    * Division
    
    * different ci badge
    
    * Few tweaks
    
    * sitemap

--- README.md
+++ README.md
@@ -9,3 +9,5 @@ Dedicated to my (then) 6 years old son Ognjen.
 I wish him to be good mathematician.
 
 Live app available at https://abacus.it-outsource.me .
+
+[![drpreag](https://circleci.com/gh/drpreag/abacus.svg?style=shield)](https://abacus.it-outsource.me)
--- src/components/Div.vue
+++ src/components/Div.vue
@@ -1,52 +1,38 @@
 <template>
   <div id="deljenje">
-    <h4>Ускоро...</h4>
-<!--
+
     <div>
-      <router-link :to="{ name:'MulBasic', params: { prop_title:'Множење бројева до 5', prop_1_min:1, prop_1_max:5, prop_2_min:2, prop_2_max:5 } }">
-        <button class="btn btn-info">Множење бројева до 5</button>
+      <router-link :to="{ name:'DivBasic', params: { prop_title:'Дељење бројева до 20', prop_1_min:2, prop_1_max:4, prop_2_min:3, prop_2_max:5 } }">
+        <button class="btn btn-info">Дељење бројева до 20</button>
       </router-link>
     </div>
     <br>
     <div>
-      <router-link :to="{ name:'MulBasic', params: { prop_title:'Множење бројева до 10', prop_1_min:2, prop_1_max:10, prop_2_min:3, prop_2_max:10 } }">
-        <button class="btn btn-info">Множење бројева до 10</button>
-      </router-link>
-    </div>
-    <br>
-    <div>
-      <router-link :to="{ name:'MulBasic', params: { prop_title:'Множење бројева до 20', prop_1_min:2, prop_1_max:20, prop_2_min:3, prop_2_max:10 } }">
-        <button class="btn btn-info">Множење бројева до 20</button>
+      <router-link :to="{ name:'DivBasic', params: { prop_title:'Дељење бројева до 100', prop_1_min:2, prop_1_max:9, prop_2_min:3, prop_2_max:10 } }">
+        <button class="btn btn-info">Дељење бројева до 100</button>
       </router-link>
     </div>
     <br><br>
     <div>
-      <router-link :to="{ name:'MulX', params: { prop_title:'Непознат број до 5', prop_1_min:2, prop_1_max:5, prop_2_min:2, prop_2_max:5 } }">
+      <router-link :to="{ name:'DivX', params: { prop_title:'Непознат број до 5', prop_1_min:2, prop_1_max:5, prop_2_min:2, prop_2_max:5 } }">
         <button class="btn btn-info">Непознат број до 5</button>
       </router-link>
     </div>
     <br>
     <div>
-      <router-link :to="{ name:'MulX', params: { prop_title:'Непознат број до 10', prop_1_min:2, prop_1_max:10, prop_2_min:2, prop_2_max:10 } }">
+      <router-link :to="{ name:'DivX', params: { prop_title:'Непознат број до 10', prop_1_min:2, prop_1_max:10, prop_2_min:2, prop_2_max:10 } }">
         <button class="btn btn-info">Непознат број до 10</button>
       </router-link>
     </div>
-    <br>
-    <div>
-      <router-link :to="{ name:'MulX', params: { prop_title:'Непознат број до 20', prop_1_min:2, prop_1_max:20, prop_2_min:5, prop_2_max:20 } }">
-        <button class="btn btn-info">Непознат број до 20</button>
-      </router-link>
-    </div>
-     -->
   </div>
 </template>
 
 <script>
 export default {
-  name: "Множење",
+  name: "Дељење",
   data () {
     return {
-      title: "Множење"
+      title: "Дељење"
     }
   }
 }
